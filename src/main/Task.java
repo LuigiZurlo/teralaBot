52f46b163e1fd0a16149187ac561af8a40ea0ba9
Luigi <luigi.zurlo01@universitadipavia.it>
2019-06-02 18:33:12 +0200

    fixed past data function

--- src/main/Task.java
+++ src/main/Task.java
@@ -32,141 +32,133 @@ import util.PostJson;
  */
 public class Task {
 
-   private static final String temperature = "air_temperature";
-   private static final String pressure = "atmospheric_pressure";
+    private static final String TEMPERATURE = "air_temperature";
+    private static final String PRESSURE = "atmospheric_pressure";
 
-   public void runPast() throws IOException {
-      ArrayList<QueryParameter> queryParameters = new ArrayList<>();
-      List<Object> locations = ManagerDB.getObjectList(Query.GET_LOCATION_TEST, queryParameters);
-//        for (Location location : ((List<Location>) (List<?>) (locations))) {
-      Location l = (Location) locations.get(0);
+    public void runPast() throws IOException {
+        ArrayList<QueryParameter> queryParameters = new ArrayList<>();
+        List<Object> locations = ManagerDB.getObjectList(Query.GET_LOCATION_TO_UPDATE, queryParameters);
+        System.out.println("Scarico misure passate per json");
+        for (Location l : ((List<Location>) (List<?>) (locations))) {
+//        Location l = (Location) locations.get(0);
+            System.out.println("PA-" + l.getSensor().getPrimaryPurpleairId());
+            Datum ric = new Datum("PA-" + l.getSensor().getPrimaryPurpleairId());
+            List<Data> parametri = new ArrayList<>();
+            for (int i = 1; i < 7; i++) {
+                queryParameters.add(new QueryParameter("location_id", l.getId()));
+                queryParameters.add(new QueryParameter("param_id", i));
 
-      Datum ric = new Datum("PA-" + l.getSensor().getPrimaryPurpleairId());
-      List<Data> parametri = new ArrayList<>();
-      for (int i = 1; i < 7; i++) {
-         queryParameters.add(new QueryParameter("location_id", l.getId()));
-         queryParameters.add(new QueryParameter("param_id", i));
-
-         List<Measurementavg> mis = (List<Measurementavg>) (List<?>) ManagerDB.getObjectList(Query.GET_SENSOR_MEASURES, queryParameters);
-         if (!mis.isEmpty()) {
-            Data variabile = null;
-            if (mis.get(0).getParameter().getId() == 4) {
-               variabile = new Data(temperature);
-//                    System.out.println("temperature");
-            } else if (mis.get(0).getParameter().getId() == 6) {
-               variabile = new Data(pressure);
-            } else {
-               variabile = new Data(mis.get(0).getParameter().getCode());
+                List<Measurementavg> mis = (List<Measurementavg>) (List<?>) ManagerDB.getObjectList(Query.GET_SENSOR_MEASURES, queryParameters);
+                if (!mis.isEmpty()) {
+                    Data variabile = new Data(getParameterName(mis.get(0).getParameter()));
+                    List<Value> valori = new ArrayList<>();
+                    for (Measurementavg mi : mis) {
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        valori.add(new Value(mi.getValore(), dateFormat.format(mi.getTimestamp())));
+                    }
+                    variabile.setValues(valori);
+                    parametri.add(variabile);
+                }
+                ric.setData(parametri);
+                Request r = new Request();
+                List<Datum> d = new ArrayList<>();
+                d.add(ric);
+                r.setData(d);
+                jsonToFile(r, "jsons/" + l.getSensor().getPrimaryPurpleairId() + "PAR"+ mis.get(0).getParameter().getCode() + ".json");
             }
-            List<Value> valori = new ArrayList<>();
+
+        }
+    }
+
+    public void runDefault() {
+        ArrayList<QueryParameter> queryParameters = new ArrayList<>();
+        List<Object> locations = ManagerDB.getObjectList(Query.GET_LOCATION_TO_UPDATE, queryParameters);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Request r = new Request();
+        List<Datum> data = new ArrayList<>();
+        for (Location l : ((List<Location>) (List<?>) (locations))) {
+            Datum ric = new Datum("PA-" + l.getSensor().getPrimaryPurpleairId());
+            System.out.println("Scarico misure sensore " + l.getSensor().getDisplayName());
+            queryParameters.add(new QueryParameter("location_id", l.getId()));
+            queryParameters.add(new QueryParameter("timestamp", DateManager.truncate(new Date().getTime(), Constants.SLICE_TIME)));
+            List<Measurementavg> mis = (List<Measurementavg>) (List<?>) ManagerDB.getObjectList(Query.GET_LAST_MEASURES, queryParameters);
+
             for (Measurementavg mi : mis) {
-               DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-               valori.add(new Value(mi.getValore(), dateFormat.format(mi.getTimestamp())));
+
+                List<Value> valori = new ArrayList<>();
+
+                valori.add(new Value(mi.getValore(), dateFormat.format(mi.getTimestamp())));
+                Data dato = new Data(getParameterName(mi.getParameter()));
+                dato.setValues(valori);
+
+                ric.addData(dato);
             }
-            variabile.setValues(valori);
-            parametri.add(variabile);
-         }
-      }
+            if (!mis.isEmpty()) {
+                data.add(ric);
+            }
+            queryParameters.clear();
+        }
 
-      ric.setData(parametri);
-      Request r = new Request();
-      List<Datum> d = new ArrayList<>();
-      d.add(ric);
-      r.setData(d);
-      postJson(r, "jsons/prova.json");
-
-//        }
-   }
-
-   public void runDefault() {
-      ArrayList<QueryParameter> queryParameters = new ArrayList<>();
-      List<Object> locations = ManagerDB.getObjectList(Query.GET_LOCATION_TO_UPDATE, queryParameters);
-      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-      Request r = new Request();
-      List<Datum> data = new ArrayList<>();
-      for (Location l : ((List<Location>) (List<?>) (locations))) {
-         Datum ric = new Datum("PA-" + l.getSensor().getPrimaryPurpleairId());
-         System.out.println("Scarico misure sensore " + l.getSensor().getDisplayName());
-         queryParameters.add(new QueryParameter("location_id", l.getId()));
-         queryParameters.add(new QueryParameter("timestamp", DateManager.truncate(new Date().getTime(), Constants.SLICE_TIME)));
-         List<Measurementavg> mis = (List<Measurementavg>) (List<?>) ManagerDB.getObjectList(Query.GET_LAST_MEASURES, queryParameters);
-
-         for (Measurementavg mi : mis) {
-
-            List<Value> valori = new ArrayList<>();
-
-            valori.add(new Value(mi.getValore(), dateFormat.format(mi.getTimestamp())));
-            Data dato = new Data(getParameterName(mi.getParameter()));
-            dato.setValues(valori);
-
-            ric.addData(dato);
-         }
-         if (!mis.isEmpty()) {
-            data.add(ric);
-         }
-         queryParameters.clear();
-      }
-
-      r.setData(data);
-      jsonToFile(r, "jsons/provaDati.json");
+        r.setData(data);
+        jsonToFile(r, "jsons/provaDati.json");
 //        postJson(r, "jsons/provaDefault.json");
 
-   }
+    }
 
-   public void createJsonSensor() {
-
-      ArrayList<QueryParameter> queryParameters = new ArrayList<>();
-      List<Location> locations = (List<Location>) (List<?>) ManagerDB.getObjectList(Query.GET_LOCATION_TO_UPDATE, queryParameters);
-      for (Location location : locations) {
-         String code = "PA-" + location.getSensor().getPrimaryPurpleairId();
-         RegisterEnvironmentDevice dev = new RegisterEnvironmentDevice(code);
-         RegisterEnvironmentDeviceLocation loc = new RegisterEnvironmentDeviceLocation(code, location.getGeolocation().getY(), location.getGeolocation().getX(), location.getElevation(), location.getSensor().getDisplayName());
-         // per file
+    public void createJsonSensor() {
+        System.out.println("this = ");
+        ArrayList<QueryParameter> queryParameters = new ArrayList<>();
+        List<Location> locations = (List<Location>) (List<?>) ManagerDB.getObjectList(Query.GET_LOCATION_TO_UPDATE, queryParameters);
+        for (Location location : locations) {
+            String code = "PA-" + location.getSensor().getPrimaryPurpleairId();
+            RegisterEnvironmentDevice dev = new RegisterEnvironmentDevice(code);
+            RegisterEnvironmentDeviceLocation loc = new RegisterEnvironmentDeviceLocation(code, location.getGeolocation().getY(), location.getGeolocation().getX(), location.getElevation(), location.getSensor().getDisplayName());
+            // per file
 //                    String root = "jsons/";
 //            jsonToFile(dev, root + RegisterEnvironmentDevice.class.getSimpleName() + code + ".json");
 //            jsonToFile(loc, root + RegisterEnvironmentDeviceLocation.class.getSimpleName() + code + ".json");  
 
-         //POST request
-         postJson(dev, constant.EndPoint.REGISTER_DEVICE);
-         postJson(loc, constant.EndPoint.REGISTER_LOCATION);
-      }
+//         POST request
+            postJson(dev, constant.EndPoint.REGISTER_DEVICE);
+            postJson(loc, constant.EndPoint.REGISTER_LOCATION);
+        }
 
-   }
+    }
 
-   private String getParameterName(Parameter2 p) {
+    private String getParameterName(Parameter2 p) {
 
-      if (p.getId() == 4) {
-         return temperature;
-      } else if (p.getId() == 6) {
-         return pressure;
-      } else {
-         return p.getCode();
-      }
+        if (p.getId() == 4) {
+            return TEMPERATURE;
+        } else if (p.getId() == 6) {
+            return PRESSURE;
+        } else {
+            return p.getCode();
+        }
 
-   }
+    }
 
-   private void postJson(Object o, String f) {
-      ObjectMapper m = new ObjectMapper();
+    private void postJson(Object o, String f) {
+        ObjectMapper m = new ObjectMapper();
 
-      try {
-         // 	String jsonString = mapper.writeValueAsString(object);
+        try {
+            // 	String jsonString = mapper.writeValueAsString(object);
 //            m.writeValue(new File(f), o);
-         PostJson.Post_JSON(f, m.writeValueAsString(o));
-      } catch (IOException ex) {
-         Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
-      }
-   }
+            PostJson.Post_JSON(f, m.writeValueAsString(o));
+        } catch (IOException ex) {
+            Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-   private void jsonToFile(Object o, String f) {
-      ObjectMapper m = new ObjectMapper();
-      try {
+    private void jsonToFile(Object o, String f) {
+        ObjectMapper m = new ObjectMapper();
+        try {
 
-         m.writeValue(new File(f), o);
+            m.writeValue(new File(f), o);
 
-      } catch (IOException ex) {
-         Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
-      }
-   }
+        } catch (IOException ex) {
+            Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }

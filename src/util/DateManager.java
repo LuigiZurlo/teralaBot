ba7dfe3ddae2b8859b032d969e4fc1a846430a95
Cristiana Larizza <cristiana.larizza@unipv.it>
2019-06-02 19:10:07 +0200

    added truncate method overloading

--- src/util/DateManager.java
+++ src/util/DateManager.java
@@ -16,4 +16,9 @@ public class DateManager {
    public static Date truncate(long time, int min) {
       return new Date(time - time % (60000 * min));
    }
+
+   public static Date truncate(Date d, int min) {
+      long time = d.getTime();
+      return new Date(time - time % (60000 * min));
+   }
 }
